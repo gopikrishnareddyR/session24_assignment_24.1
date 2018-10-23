ca12f97cdd3eabd241a8379abd16cd0e1c53691c
gopikrishnareddyR <40296528+gopikrishnareddyR@users.noreply.github.com>
2018-10-23 22:22:34 +0530

    Add files via upload

---
+++ session24_assignment_24_1.R
@@ -0,0 +1,10 @@
+#session24_assignment_24.1
+
+
+#1. perform the below given activities
+
+#a.Take a sample dataset of your choice
+
+#b. Apply random forest , logistic regression using spark R
+
+#c. Predict for new dataset
